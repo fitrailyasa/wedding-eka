b8d08ad7e3dcd98a87ad85d0e3242bad5c8ef696
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-08-06 00:23:20 +0700

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,22 +1,22 @@
 <section class="section-container">
-    <div class="container py-5 my-5">
-        <p class="lead animate-text delay-1" style="padding-bottom: 30px">
+    <div class="container mt-3">
+        <p class="lead animate-text delay-1" style="padding-bottom: 20px">
             THE WEDDING OF
         </p>
         <h1 class="font-wedding display-3 animate-text delay-2" style="font-size: 60px">
             Eka<br />&<br />Wildan
         </h1>
-        <div style="margin-top: 100px">
+        <div style="margin-top: 30px">
             <a href="#bannerCarousel" id="open-btn"
                 class="btn btn-light rounded-pill px-4 py-2 animate__animated animate__pulse animate__infinite">
-                Buka Undangan
+                Open Invitation
             </a>
         </div>
         <div class="d-flex justify-content-center pb-5">
             <div class="my-5 rounded py-3 px-5 animate-text delay-3">
-                <p class="py-0 my-0">Kepada Yth.</p>
-                <p class="fw-bold py-0 my-0" id="guest-name">Tamu Undangan</p>
-                <p class="py-0 my-0">Ditempat</p>
+                <p class="py-0 my-0">Dear:</p>
+                <p class="fw-bold py-0 my-0" id="guest-name">Guest</p>
+                <p class="py-0 my-0">You are</p>
             </div>
         </div>
     </div>
