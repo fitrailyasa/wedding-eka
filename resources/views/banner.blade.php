ed79b1cce4262489cc339414b0561edfdccbb985
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-08-08 04:03:47 +0700

    update

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -7,23 +7,10 @@
 
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="{{ asset('') }}assets/photo (1).JPG" class="d-block w-100 banner-img" alt="Banner 1" />
-                <div class="carousel-caption top-caption">
-                    <p class="text-shadow m-0 p-0" style="font-family: 'Lovelace', serif;">THANK YOU</p>
-                    <p class="text-shadow m-0 p-0" style="font-family: 'Lovelace-Bold', serif; font-size: 44px;">FOR</p>
-                    <p class="text-shadow m-0 p-0" style="font-family: 'Lovelace-Bold', serif; font-size: 44px;">BEING</p>
-                    <p class="text-shadow m-0 p-0" style="font-family: 'Lovelace-Bold', serif; font-size: 44px;">A</p>
-                    <p class="text-shadow m-0 p-0" style="font-family: 'Lovelace-Bold', serif; font-size: 44px;">PART</p>
-                    <p class="text-shadow m-0 p-0" style="font-family: 'Lovelace-Bold', serif; font-size: 44px;">OF</p>
-                    <p class="text-shadow" style="font-family: 'Lovelace', serif;">OUR HISTORY</p>
-                </div>
+                <img src="{{ asset('') }}assets/banner-1-slide-2.JPG" class="d-block w-100 banner-img" alt="Banner 1" />
             </div>
             <div class="carousel-item">
-                <img src="{{ asset('') }}assets/photo (2).JPG" class="d-block w-100 banner-img" alt="Banner 2" />
-                <div class="carousel-caption top-caption">
-                    <p class="text-shadow the-of-text">“Loved you yesterday, love you still,<br> always have, always will.” - Elaine Davis</p>
-                    <p class="text-shadow" style="font-family: 'Glacial Indifference', sans-serif;">#WEddingparty</p>
-                </div>
+                <img src="{{ asset('') }}assets/banner-2-slide-2.JPG" class="d-block w-100 banner-img" alt="Banner 2" />
             </div>
         </div>
 
